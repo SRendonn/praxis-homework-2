f35e48e17e6a2c67ed5b8c1873034eb8a7967037
Sebas R <sebasrg0505@gmail.com>
2021-11-01 01:50:52 -0500

    build app bash file

---
+++ build-app/README.md
@@ -0,0 +1,4 @@
+# Environment variables
+
+* Run ``vagrant plugin install vagrant-env``.
+* To load the ``IPSTACK_API_KEY`` variable, create a ``.env`` file in this directory with the next line: ``IPSTACK_API_KEY=<Your API key here>``.
--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,35 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+# install git if not already installed
+sudo yum install git -y
+
+# install Golang
+wget -nc -O go.tar.gz https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+# extract tar ball
+sudo tar -C /usr/local -xzf go.tar.gz
+# add folder to path
+export PATH=$PATH:/usr/local/go/bin
+
+# download nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+# reload PATH
+source ~/.bash_profile
+# install node 14.x
+nvm install --lts=fermium
+# install vue cli
+npm install -g @vue/cli
+
+mkdir -p ~/app
+cd ~/app
+# clone repo into current dir
+git clone https://github.com/jdmendozaa/vuego-demoapp.git .
+# build go app
+cd ~/app/server
+sudo mkdir -p /shared/server
+sudo go build -o /shared/server
+# install vue app dependencies
+cd ~/app/spa
+# install vue cli globally and add eslint plugin to fix vue-cli-service bug
+npm install
+# build production app and move to shared directory
+npm run build
+sudo mv dist /shared/
