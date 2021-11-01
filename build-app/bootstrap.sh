dbff44a83f050c074fe401f68354ba881bb69246
Sebas R <sebasrg0505@gmail.com>
2021-11-01 15:05:14 -0500

    improved build-app script

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # install git if not already installed
-sudo yum install git -y
+yum install git -y
 
 # install Golang
 wget -nc -O go.tar.gz https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
 # extract tar ball
-sudo tar -C /usr/local -xzf go.tar.gz
+tar -C /usr/local -xzf go.tar.gz
 # add folder to path
 export PATH=$PATH:/usr/local/go/bin
 
@@ -15,23 +15,34 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bash_profile
 # install node lts
 nvm install --lts=fermium
-# install vue cli
+# install yarn package manager
 npm install -g yarn
-
+# install vue cli
 yarn global add @vue/cli
-
-mkdir -p ~/app
+# /app directory where app will be cloned
+rm -rf ~/app
+mkdir ~/app
 cd ~/app
 # clone repo into current dir
 git clone https://github.com/jdmendozaa/vuego-demoapp.git .
-# build go app
+# build go server
 cd ~/app/server
-sudo mkdir -p /shared/server
-sudo go build -o /shared/server
-# install vue app dependencies
+mkdir -p /shared/server
+# in theory it should get the PORT and IPSTACK_API_KEY variables from the environment
+go build -o /shared/server
+
 cd ~/app/spa
-# install vue cli globally and add eslint plugin to fix vue-cli-service bug
+# generate yarn.lock from package-lock.json
+yarn import
+# remove package-lock.json
+rm -f package-lock.json
+# update dependencies
+yarn upgrade
+# install vue app dependencies
 yarn install
+# get the VUE_APP_API_ENDPOINT from environment and write it to .env file
+echo $VUE_APP_API_ENDPOINT > .env
 # build production app and move to shared directory
 yarn build
-sudo mv dist /shared/
+rm -rf /shared/dist
+mv dist /shared/
--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-echo "Hello from backend!"
+# copy server binary from /shared directory to /app directory
+# may skip this step
+mkdir -p /app/go-server
+cp /shared/server /app/go-server
+cd /app/go-server
+# run the binary, & makes it run in the background
+./vuego-demoapp &
