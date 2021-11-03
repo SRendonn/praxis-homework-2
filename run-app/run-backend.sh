a05253376d40668b56045d4072ce4a9ef5a8c9d3
Sebas R <sebasrg0505@gmail.com>
2021-11-03 13:55:19 -0500

    idempotent run-frontend and run-backend

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# copy server binary from /shared directory to /app directory
-# may skip this step
-mkdir -p /app
-cp -R /shared/server /app
 # create systemd unit file for start on boot
 cd /etc/systemd/system
 echo "[Unit]" > go-server.service
@@ -13,6 +9,13 @@ echo "Environment=\"IPSTACK_API_KEY=$IPSTACK_API_KEY\"" >> go-server.service
 echo "ExecStart=/app/server/vuego-demoapp &" >> go-server.service
 echo "[Install]" >> go-server.service
 echo "WantedBy=multi-user.target" >> go-server.service
-# enable go-server.service on startup and start it with --now
+# reload systemd configuration
 systemctl daemon-reload
+# stop so the executable can be overwritten
+systemctl --now disable go-server.service
+# create /app dir where server will be placed
+rm -rf /app
+mkdir /app
+cp -R /shared/server /app
+# enable go-server.service on startup and start it with --now
 systemctl --now enable go-server.service
--- run-app/run-frontend.sh
+++ run-app/run-frontend.sh
@@ -2,3 +2,64 @@
 yum install nginx -y
 
 systemctl --now enable nginx
+
+rm -rf /app
+mkdir /app
+cp /shared/spa/vue_dist.tar.gz /app/vue_dist.tar.gz
+cd /app
+tar -zxf vue_dist.tar.gz
+
+mkdir -p /etc/ssl/private
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vuego-demoapp.key -out /etc/ssl/certs/vuego-demoapp.crt \
+-subj "/C=CO/ST=Antioquia/L=Medellin/O=Praxis_Perficient/CN=localhost"
+
+cat <<-'default_config' > /etc/nginx/nginx.conf
+user  nginx;
+worker_processes  1;
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+events {
+  worker_connections  1024;
+}
+http {
+  include       /etc/nginx/mime.types;
+  default_type  application/octet-stream;
+  log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                    '$status $body_bytes_sent "$http_referer" '
+                    '"$http_user_agent" "$http_x_forwarded_for"';
+  access_log  /var/log/nginx/access.log  main;
+  sendfile        on;
+  keepalive_timeout  65;
+  server {
+    listen       80;
+    server_name  localhost;
+    location / {
+      root   /app/dist;
+      index  index.html;
+      try_files $uri $uri/ /index.html;
+    }
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+      root   /usr/share/nginx/html;
+    }
+  }
+  server {
+    listen       443 ssl;
+    server_name  localhost;
+    ssl_certificate /etc/ssl/certs/vuego-demoapp.crt;
+    ssl_certificate_key /etc/ssl/private/vuego-demoapp.key;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    location / {
+      root   /app/dist;
+      index  index.html;
+      try_files $uri $uri/ /index.html;
+    }
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+      root   /usr/share/nginx/html;
+    }
+  }
+} 
+default_config
+
+systemctl reload nginx
